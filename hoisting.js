689e4fecb55aa8bd3e95f1870bdc94e68d7824f5
Michelle Sage <michelle2sage@gmail.com>
2019-11-05 02:10:38 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,16 @@
-function callMe() {
+var lyric
+lyric()
+lyric = function () {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  var lyric = "maybe"
 }
-
+return lyric
 
 function crazy() {
-  // fix the code in here:
   thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
